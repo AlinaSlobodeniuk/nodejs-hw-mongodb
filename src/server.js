893dcb91bd467b8909aa801bd2070ae979bdab4d
Alina Slobodeniuk <112395038+AlinaSlobodeniuk@users.noreply.github.com>
2025-09-09 02:11:43 +0300

    fix hw3-crud

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ import pino from 'pino-http';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import { getEnvVar } from './utils/getEnvVar.js';
-import { contactsRouter } from './routers/contacts.js';
+import router from './routers/contacts.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 
@@ -25,9 +25,9 @@ export const setupServer = () => {
     }),
   );
 
-  app.use(contactsRouter);
+  app.use(router);
 
-  app.use('*', notFoundHandler);
+  app.use('/', notFoundHandler);
 
   app.use(errorHandler);
 
