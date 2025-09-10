1f49aa3a5e973e3ab698edb31d49dedb201b4458
Alina Slobodeniuk <112395038+AlinaSlobodeniuk@users.noreply.github.com>
2025-09-10 15:05:21 +0300

    fix router

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -1,20 +1,46 @@
 import { Router } from 'express';
-import { createContactsController, deleteContactController, getContactsByIdController, getContactsController, patchContactsController } from '../controllers/contacts.js';
+import {
+  createContactsController,
+  deleteContactController,
+  getContactsByIdController,
+  getContactsController,
+  patchContactsController,
+} from '../controllers/contacts.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 import { validateBody } from '../middlewares/validateBody.js';
-import { createContactsSchema, updateContactSchema } from '../validation/contacts.js';
+import {
+  createContactsSchema,
+  updateContactSchema,
+} from '../validation/contacts.js';
 import { isValidId } from '../middlewares/isValidId.js';
 
 const router = Router();
 
-  router.get('/contacts', ctrlWrapper(getContactsController));
+router.get('/contacts', ctrlWrapper(getContactsController));
 
-   router.get('/contacts/:contactId', isValidId, ctrlWrapper(getContactsByIdController));
+router.get(
+  '/contacts/:contactId',
+  isValidId,
+  ctrlWrapper(getContactsByIdController),
+);
 
-   router.post('/contacts', validateBody(createContactsSchema), ctrlWrapper(createContactsController));
+router.post(
+  '/contacts',
+  validateBody(createContactsSchema),
+  ctrlWrapper(createContactsController),
+);
 
-   router.patch('/contacts/:contactId', validateBody(updateContactSchema), isValidId, ctrlWrapper(patchContactsController));
+router.patch(
+  '/contacts/:contactId',
+  validateBody(updateContactSchema),
+  isValidId,
+  ctrlWrapper(patchContactsController),
+);
 
-   router.delete('/contacts/:contactId', isValidId, ctrlWrapper(deleteContactController));
+router.delete(
+  '/contacts/:contactId',
+  isValidId,
+  ctrlWrapper(deleteContactController),
+);
 
 export default router;
