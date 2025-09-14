a434989212b4ad2cac3e02b910c45359c144d709
Alina Slobodeniuk <112395038+AlinaSlobodeniuk@users.noreply.github.com>
2025-09-14 23:41:15 +0300

    hw7-swagger done

--- src/server.js
+++ src/server.js
@@ -8,6 +8,7 @@ import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import cookieParser from 'cookie-parser';
 import { UPLOAD_DIR } from './constants/index.js';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 dotenv.config();
 
@@ -27,7 +28,8 @@ export const setupServer = () => {
     }),
   );
 
-  app.use('/uploads', express.static(UPLOAD_DIR));
+  app.use('/uploads', express.static(UPLOAD_DIR)); 
+  app.use('/api-docs', swaggerDocs());
 
   app.use(router);
 
