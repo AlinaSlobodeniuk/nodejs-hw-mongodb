ced325f2e3f15ad613650d7e02f0b8b42611740d
Alina Slobodeniuk <112395038+AlinaSlobodeniuk@users.noreply.github.com>
2025-10-14 03:31:58 +0300

    parse to boolean

---
+++ src/middlewares/parseToBoolean.js
@@ -0,0 +1,5 @@
+export const parseToBoolean = (req, res, next) => {
+  if (req.body.isFavourite === 'true') req.body.isFavourite = true;
+  if (req.body.isFavourite === 'false') req.body.isFavourite = false;
+  next();
+};
